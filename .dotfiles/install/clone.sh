5dc87c0918cd0185a9aef9aa978730b5706055f9
Greg Lamb <email@greglamb.me>
2022-01-08 15:23:28 -0800

    fixed clone script

--- .dotfiles/install/clone.sh
+++ .dotfiles/install/clone.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+backup="$HOME/.dotfiles.backup-"`date +"%Y%m%d_%H%M%S"`
 cd $HOME
+mkdir -p $backup
+mv $HOME/.dotfiles.git $backup/
 git clone --bare git@github.com:greglamb/dotfiles.git $HOME/.dotfiles.git
 dtgit() { /usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME $@ }
 mvdir() { mkdir -p "${@: -1}" && mv "$@"; }
-mkdir -p .predt-backup
 dtgit checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    dtgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mvdir {} .predt-backup/{}
+    dtgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mvdir {} $backup/{}
 fi;
 dtgit checkout
 dtgit config status.showUntrackedFiles no
