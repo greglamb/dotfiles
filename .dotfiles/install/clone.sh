04bfba1788ab4bbaa1d58b69aca8dd5a12002dfd
Greg Lamb <email@greglamb.me>
2022-01-08 23:26:20 -0800

    yep

--- .dotfiles/install/clone.sh
+++ .dotfiles/install/clone.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
-dtgit() { /usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME "$@" }
-mvdir() { mkdir -p "${@: -1}" && mv "$@"; }
+function dtimvdir {
+  echo moving "$1"
+  mkdir -p "${@: -1}" && mv "$@"
+}
 
-backup="$HOME/.dotfiles.backup-"`date +"%Y%m%d_%H%M%S"`
+function dtgit {
+  /usr/bin/git --git-dir=$HOME/.dotfiles.git/ --work-tree=$HOME "$@"
+}
+
+dtibackupdir="$HOME/.dotfiles.backup-"`date +"%Y%m%d_%H%M%S"`
 cd $HOME
-mkdir -p $backup
-mv $HOME/.dotfiles.git $backup/
+mkdir -p $dtibackupdir
+mv $HOME/.dotfiles.git $dtibackupdir/
 git clone --bare git@github.com:greglamb/dotfiles.git $HOME/.dotfiles.git
 dtgit checkout
 if [ $? = 0 ]; then
   echo "Checked out config.";
   else
     echo "Backing up pre-existing dot files.";
-    dtgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mvdir {} $backup/{}
+    export -f dtimvdir
+    export dtibackupdir
+    dtgit checkout 2>&1 | egrep "^\s" | awk {'print $1'} | xargs -I{} bash -c 'dtimvdir "{}" "$dtibackupdir"/"{}"'
 fi;
 dtgit checkout
 dtgit config status.showUntrackedFiles no
+
+        
