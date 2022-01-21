80a3edbc211ca1dc4a5886b84c5792821b994972
Greg Lamb <glamb@launchcg.com>
2022-01-20 17:05:45 -0800

    sync

--- .dotfiles/fish/source/source.d/wsl2-env.fish
+++ .dotfiles/fish/source/source.d/wsl2-env.fish
@@ -1,5 +1,5 @@
 if [ $DFWSL = "2" ]
-    if not test -e ~/cmd.exe
+    if not test -e ~/bin/cmd.exe
         echo 'ERROR: Copy cmd.exe to the bin directory in your Users profile, and then create a symbolic link to it in ~/bin'
     else
 
