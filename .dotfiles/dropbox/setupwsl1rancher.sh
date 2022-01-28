42f07f7c6ed61cc05b4624d582e94f422e651cf8
Greg Lamb <glamb@launchcg.com>
2022-01-28 09:01:42 -0800

    sync

---
+++ .dotfiles/dropbox/setupwsl1rancher.sh
@@ -0,0 +1,3 @@
+chmod 755 "/mnt/c/Users/GLamb/AppData/Local/Programs/Rancher Desktop/resources/resources/linux/bin"/*
+chmod 755 "/mnt/c/Users/GLamb/AppData/Local/Programs/Rancher Desktop/resources/resources/linux/wsl-helper"
+
