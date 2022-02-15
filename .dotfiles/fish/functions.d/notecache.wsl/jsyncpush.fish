665af76afacc6e99ce540c1ca666138809e5dac0
Greg Lamb <glamb@launchcg.com>
2022-02-15 15:21:07 -0800

    sync

--- .dotfiles/fish/functions.d/notecache.wsl/jsyncpush.fish
+++ .dotfiles/fish/functions.d/notecache.wsl/jsyncpush.fish
@@ -1,5 +1,7 @@
 function jsyncpush
-    git -C $WINHOME/jrnl add work.txt
-    git -C $WINHOME/jrnl commit -m "sync"
-    git -C $WINHOME/jrnl push
+    if test -d $WINHOME/jrnl/.git
+        git -C $WINHOME/jrnl add work.txt
+        git -C $WINHOME/jrnl commit -m "sync"
+        git -C $WINHOME/jrnl push
+    end
 end
