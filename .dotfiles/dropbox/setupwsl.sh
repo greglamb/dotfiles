201980ad0727a8883109a682186829957a25c63e
Greg Lamb <glamb@launchcg.com>
2022-02-18 11:29:34 -0800

    sync

--- .dotfiles/dropbox/setupwsl.sh
+++ .dotfiles/dropbox/setupwsl.sh
@@ -10,3 +10,6 @@ ln -s /mnt/c/Users/GLamb/bin/explorer.exe ~/bin/explorer.exe
 cp /mnt/c/windows/system32/cmd.exe /mnt/c/Users/GLamb/bin/
 chmod 755 /mnt/c/Users/GLamb/bin/cmd.exe
 ln -s /mnt/c/Users/GLamb/bin/cmd.exe ~/bin/cmd.exe
+
+
+#### don't alter permissions any more - it's not worth it
